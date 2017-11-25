bfb4295c979314c477dee86403aff521aa181371
Dawson M <dawson.mortenson@gmail.com>
2017-11-24 23:42:20 -0800

    setup user and address models, incorperate boostrap styling, and make a landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def import
+    Address.import(params[:file])
+    redirect_to root_url, notice: "It worked!"
+  end
 end
